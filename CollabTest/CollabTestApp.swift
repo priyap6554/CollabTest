80076ea9c5646fbb8a5027b977ff964fa8037336
Priya <priyapatel7989@gmail.com>
2023-08-15 12:11:02 -0500

    Initial Commit

---
+++ CollabTest/CollabTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollabTestApp.swift
+//  CollabTest
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CollabTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
